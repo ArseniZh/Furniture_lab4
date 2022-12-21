ede3ef71d7dc0633a85014cbd6410beee94f3878
ArseniZh <szhirnevich@mail.ru>
2022-12-22 01:35:03 +0300

    add (Cupboard): add field "assembled"

--- src/furniture/Cupboard.java
+++ src/furniture/Cupboard.java
@@ -8,6 +8,8 @@ public class Cupboard implements IFurnitureAction{
     private double width;
     private double weight;
 
+    private boolean assembled;
+
     public Cupboard() {
         this.xCoordinate = 0;
         this.yCoordinate = 0;
@@ -15,18 +17,21 @@ public class Cupboard implements IFurnitureAction{
         this.height = 0;
         this.width = 0;
         this.weight = 0;
+        this.assembled = false;
     }
 
-    public Cupboard(double xCoordinate, double yCoordinate, String color, double height, double width, double weight) {
+    public Cupboard(double xCoordinate, double yCoordinate, String color, double height, double width, double weight, boolean assembled) {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
         this.color = color;
         this.height = height;
         this.width = width;
         this.weight = weight;
+        this.assembled = assembled;
     }
 
-    public void move(double xCoordinate, ) {
-
+    public void move(double xCoordinate, double yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
     }
 }
