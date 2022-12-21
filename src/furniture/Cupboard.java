c0d39d67c81b9800a01f3de156745ecdf4c1410e
ArseniZh <szhirnevich@mail.ru>
2022-12-22 01:30:28 +0300

    add (Cupboard): add fields and two constructors

--- src/furniture/Cupboard.java
+++ src/furniture/Cupboard.java
@@ -1,5 +1,32 @@
 package furniture;
 
 public class Cupboard implements IFurnitureAction{
+    private double xCoordinate;
+    private double yCoordinate;
+    private String color;
+    private double height;
+    private double width;
+    private double weight;
 
+    public Cupboard() {
+        this.xCoordinate = 0;
+        this.yCoordinate = 0;
+        this.color = "colorless";
+        this.height = 0;
+        this.width = 0;
+        this.weight = 0;
+    }
+
+    public Cupboard(double xCoordinate, double yCoordinate, String color, double height, double width, double weight) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.color = color;
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+    }
+
+    public void move(double xCoordinate, ) {
+
+    }
 }
