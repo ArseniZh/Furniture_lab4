392c342a5ce0eaa93093defff4e4b415890b3acc
ArseniZh <szhirnevich@mail.ru>
2022-12-23 00:00:31 +0300

    add (BookCase): new fields and two constructors

--- src/furniture/BookCase.java
+++ src/furniture/BookCase.java
@@ -1,5 +1,18 @@
 package furniture;
 
 public class BookCase extends Cupboard{
+    private int bookCapacity;
+    private int shelvesNumber;
 
+    public BookCase() {
+        super();
+        this.bookCapacity = 0;
+        this.shelvesNumber = 0;
+    }
+
+    public BookCase(double xCoordinate, double yCoordinate, String color, double height, double width, double weight, boolean assembled, int bookCapacity, int shelvesNumber) {
+        super(xCoordinate, yCoordinate, color, height, width, weight, assembled);
+        this.bookCapacity = bookCapacity;
+        this.shelvesNumber = shelvesNumber;
+    }
 }
