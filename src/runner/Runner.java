03c8f06abca19241719310fb3c270b1f51e7775c
ArseniZh <szhirnevich@mail.ru>
2022-12-23 00:13:35 +0300

    add (Runner): add object of class BookCase

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -1,7 +1,10 @@
 package runner;
 
+import furniture.BookCase;
+
 public class Runner {
     public static void main(String[] args) {
-
+        BookCase bookcase = new BookCase(12.3, -11.5, "brown", 100, 45, 15, true, 74, 5);
+        System.out.println(bookcase);
     }
 }
